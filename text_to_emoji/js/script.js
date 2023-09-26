f6880d859b5d3816cb931c3c7b0871442b36697e
Lucky <luckykashyap3430@gmail.com>
2023-09-26 15:49:37 +0530

    encrypt data

--- text_to_emoji/js/script.js
+++ text_to_emoji/js/script.js
@@ -9,23 +9,23 @@ function encryption() {
     // get the password
 
     var pass = document.getElementById("pass").value;
-    console.log(pass);
+    // console.log(pass);
 
     // get the input
 
     var input = document.getElementById("text-msg").value;
-    console.log(input);
+    // console.log(input);
     // splitting the input
     const str = input.split("");
 
-    console.log(str);
+    // console.log(str);
 
     // convert it into imojis
     str.forEach((ele) => {
       addData += `&#128${ele.charCodeAt()}`;
     });
 
-    console.log(addData);
+    // console.log(addData);
 
     document.querySelector(".result").style.display = "block";
 
@@ -39,7 +39,7 @@ function encryption() {
 
     if (JSON.parse(localStorage.getItem("data1"))) {
       data = JSON.parse(localStorage.getItem("data1"));
-      console.log(data);
+    //   console.log(data);
       data.push({ 'pass': pass, 'text': input, 'add': addData });
     } else {
       data = [{ 'pass': pass, 'text': input, 'add': addData }];
@@ -49,6 +49,41 @@ function encryption() {
   });
 }
 
+function decryption() {
+    
+    document.querySelector("#decrypt-btn").addEventListener("click", function () {
+        var data2 = '';
+        var text2 = document.querySelector("#emoji-msg").value
+        var finalPass = document.querySelector("#password").value;
+        var user = JSON.parse(localStorage.getItem('data1'));
+        // console.log(user)
+        var str2 = text2.split(" ");
+        str2.forEach(ele => {
+                data2 += `&#${(ele.codePointAt(0))}`
+                // console.log((element.charCodeAt()) * Math.floor(Math.random() * 10))
+        });
+        console.log(data2);
+        var found;
+        for(let i of user){
+            if(i.addData == data2){
+                found = i;
+                // console.log(i)
+            }
+        }
+        if (found.addData === data2) {
+            // console.log("jay ho")
+            document.querySelector(".result").style.display = `block`
+            document.querySelector(".result").style.color = `#eee`
+
+            document.querySelector(".result").innerHTML = found.text;
+        } else {
+            document.querySelector(".result").style.display = `block`
+            document.querySelector(".result").style.color = `red`
+            document.querySelector(".result").innerHTML = "Wrong password!"
+        }
+    })
+
+}
 function btnClicking() {
   document.querySelector(".decrypt").addEventListener("click", function () {
     document.querySelector(".decryption").style.display = "block";
@@ -79,4 +114,7 @@ function btnClicking() {
 }
 
 btnClicking();
+
 encryption();
+
+decryption();
