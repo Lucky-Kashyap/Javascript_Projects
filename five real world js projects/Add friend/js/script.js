edece3e475e07e4f2e1fa863fb4f1223dd1c6437
Lucky <luckykashyap3430@gmail.com>
2023-11-25 10:38:29 +0530

    add friend feature with same button

--- five real world js projects/Add friend/js/script.js
+++ five real world js projects/Add friend/js/script.js
@@ -5,12 +5,28 @@
 var isStatus = document.querySelector("h5");
 
 var add = document.querySelector(".add");
-var remove = document.querySelector(".remove");
+
+var check = 0;
+// var remove = document.querySelector(".remove");
 
 add.addEventListener("click", () => {
-  isStatus.innerHTML = "Friends";
+  if (check === 0) {
+    isStatus.innerHTML = "Friends";
+    add.innerHTML = "Remove Friend";
+    isStatus.style.color = "green";
+    check = 1;
+  } else {
+    isStatus.innerHTML = "Stranger";
+    add.innerHTML = "Add Friend";
+    isStatus.style.color = "red";
+    check = 0;
+  }
+  // isStatus.innerHTML = "Friends";
+  // add.innerHTML = "Remove";
+  // isStatus.style.color = "green";
+  // isStatus.classList.remove("hide");
 });
 
-remove.addEventListener("click", () => {
-  isStatus.innerHTML = "Stranger";
-});
+// remove.addEventListener("click", () => {
+//   isStatus.innerHTML = "Stranger";
+// });
