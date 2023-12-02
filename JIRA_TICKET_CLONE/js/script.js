0a436bdb0c0820febda6107124ddf5c4b6d44e5e
Lucky <luckykashyap3430@gmail.com>
2023-12-02 15:15:24 +0530

    modal container created

---
+++ JIRA_TICKET_CLONE/js/script.js
@@ -0,0 +1,13 @@
+// generate tickt
+
+// select modal
+
+var modal = document.querySelector(".wrapper-modal");
+var generateTicket = document.querySelector(
+  ".container .btn-container .add-btn i"
+);
+
+generateTicket.addEventListener("click", () => {
+  // modal.classList.add(".hide");
+  modal.style.display = "block";
+});
