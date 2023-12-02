0ab022cdc083827eac24b43f878dd73415accb35
Lucky <luckykashyap3430@gmail.com>
2023-12-02 15:38:22 +0530

    modal functionality build hide & show

--- JIRA_TICKET_CLONE/js/script.js
+++ JIRA_TICKET_CLONE/js/script.js
@@ -7,7 +7,22 @@ var generateTicket = document.querySelector(
   ".container .btn-container .add-btn i"
 );
 
+let addFlag = false;
+
 generateTicket.addEventListener("click", () => {
+  // Display Modal
+  // Generate Ticket
+
+  // add flag = true -> Modal Display
+  // add flag = false -> Modal Display none
+
   // modal.classList.add(".hide");
-  modal.style.display = "block";
+
+  if (addFlag != true) {
+    modal.style.display = "block";
+    addFlag = true;
+  } else {
+    modal.style.display = "none";
+    addFlag = false;
+  }
 });
