4356ffb4f6a9762295464e06e554e53851691fb5
Lucky <luckykashyap3430@gmail.com>
2023-12-05 17:26:36 +0530

    record video

--- Web Cam/js/script.js
+++ Web Cam/js/script.js
@@ -1,6 +1,15 @@
 // write code for web cam
 
 let video = document.querySelector("video");
+let recordVideo = document.querySelector(".record-btn");
+let recordPhoto = document.querySelector(".capture-btn");
+
+let recordVideoBtn = document.querySelector(".record-video");
+let recordPhotoBtn = document.querySelector(".record-photo");
+
+let recorder;
+
+let recordFlag = false;
 
 let constraints = {
   video: true,
@@ -9,4 +18,22 @@ let constraints = {
 
 navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
   video.srcObject = stream;
+
+  recorder = new MediaRecorder(stream);
+});
+
+recordVideo.addEventListener("click", () => {
+  if (!recorder) return;
+
+  recordFlag = !recordFlag;
+
+  if (recordFlag) {
+    // start
+    recorder.start();
+    recordVideoBtn.classList.add("scale-record");
+  } else {
+    // stop
+    recorder.stop();
+    recordVideoBtn.classList.remove("scale-record");
+  }
 });
